9e7befedb6c5832e8a7baab6493110015bab86f1
Tamyres Bezerraa <tamyresbezerraa@gmail.com>
2020-12-13 15:20:00 -0300

    First commit, v0 simulador

---
+++ Assets/Scripts/DeteccaoCabeca.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Autora: Tamyres Bezerra de Souza
+ */
+
+using UnityEngine;
+using VRTK;
+
+//Classe responsável por tratar os eventos do ponteiro (laser) que sai da cabeça do usuário do simulador
+public class DeteccaoCabeca : MonoBehaviour
+{
+    //ponteiro em que serão extraídos os eventos
+    public VRTK_DestinationMarker pointer;
+    //variável que faz referência a o texto que mostra a distância na tela
+    public UnityEngine.UI.Text distance;
+    //variável que faz referência a o texto que mostra o nome do objeto mirado na tela
+    public UnityEngine.UI.Text objectName;
+    //variável que faz referência a o texto que mostra a licalização do observador na tela
+    public UnityEngine.UI.Text targetLocation;
+
+    //método responsável por iniciar os métodos de eventos disparados pelo script do ponteiro (laser)
+    protected virtual void OnEnable()
+    {
+        //obtendo o ponteiro usado no simulador a partir do script usado no componente da biblioteca (VRTK)
+        pointer = (pointer == null ? GetComponent<VRTK_DestinationMarker>() : pointer);
+
+        //Caso o ponteiro exista, vamos obter os eventos de hover do ponteiro
+        if (pointer != null)
+        {
+            pointer.DestinationMarkerHover += DestinationMarkerHover;
+        }
+        else
+        {
+            //caso não exista, vamos emitir um alerta de erro
+            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTKExample_PointerObjectHighlighterActivator", "VRTK_DestinationMarker", "the Controller Alias"));
+        }
+    }
+
+    //método responsável por remover os métodos de eventos disparados pelo script do ponteiro (laser)
+    protected virtual void OnDisable()
+    {
+        if (pointer != null)
+        {
+            pointer.DestinationMarkerHover -= DestinationMarkerHover;
+        }
+    }
+
+    //método que é disparado a cada frame no simulador
+    private void Update()
+    {
+        //aqui atualizamos o texto de localização do usuário na tela, obtendo o x,y e z do mesmo
+        targetLocation.text = "x: " + transform.position.x.ToString("F") +
+            " y: " + transform.position.y.ToString("F") +
+            " z: " + (transform.position.z - 0.7).ToString("F");
+    }
+
+    //evento disparado toda vez que o ponteiro mirar em algum objeto na tela, e também é disparado ao se mover na superfície do mesmo objeto
+    private void DestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
+    {
+        //se o evento está ativo, vamos setar os textos da distância e do nome do objeto na tela
+       SetTexts(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER HOVER", e.target, e.raycastHit, e.distance, e.destinationPosition);
+    }
+
+    //método responsável por modificar os textos de distancia entre o usuário e o objeto mirado e o nome do objeto mirado
+    private void SetTexts(uint index, string action, Transform target, RaycastHit raycastHit, float distance, Vector3 tipPosition)
+    {
+        //obtendo o nome do objeto mirado, caso não exista, é retornado <NO VALID TARGET>
+        string targetName = (target ? target.name : "<NO VALID TARGET>");
+        //uma condição para mostrar a informação na tela, que é apenas no caso em que o binóculo esteja em uso pelo usuário do simulador
+        if (InteracaoBinoculo.zoomIn)
+        {
+            //caso esteja sendo usado, é modificado o texto da distancia e nome do objeto
+            this.distance.text = this.GetDistance(distance);
+            objectName.text = targetName;
+        } else
+        {
+            //caso não esteja sendo usado, são limpados os textos
+            this.distance.text = "";
+            objectName.text = "";
+        }
+    }
+
+    //método responsável por formatar a distância (texto mostrado na tela do usuário)
+    private string GetDistance(float distance)
+    {
+        double result = ((distance - 0.7) * 10);
+        if (result > 1000)
+        {
+            return (result / 1000).ToString("F") + " Km";
+        }
+        return (result).ToString("F") + " Metros";
+    }
+}
---
+++ Assets/Scripts/InteracaoBinoculo.cs
@@ -0,0 +1,124 @@
+﻿/*
+ * Autora: Tamyres Bezerra de Souza
+ */
+
+using UnityEngine;
+using UnityEngine.XR;
+using VRTK;
+
+//Classe responsável por controlar a interação entre o usuário e o binóculo
+public class InteracaoBinoculo : MonoBehaviour
+{
+    //obtenção do script de interação do (VRTK) para analisar se o binóculo está sendo segurado ou não
+    public VRTK_InteractableObject linkedObject;
+    //obtenção do script seguir do (VRTK) para controlar se o binóculo ficará suspenso sobre o peitoral
+    public VRTK_TransformFollow transformFollow;
+    //Objeto responsável por ativar ou não a borda delimitadora do binóculo (a parte escura)
+    public GameObject binoculoCamera;
+    //Obtenção do modelo da mão esquerda do (VRTK), utilizado para deixa-lá invisível para não atrapalhar a utilização do binóculo
+    public GameObject maoEsquerda;
+    //Obtenção do modelo da mão direita do (VRTK), utilizado para deixa-lá invisível para não atrapalhar a utilização do binóculo
+    public GameObject maoDireita;
+    //Obtenção da Câmera do simulador para a aplicação do zoom
+    public Camera cameraSet;
+    //multiplicador de zoom da câmera
+    private int smoth = 100;
+    //variável que sinaliza se o binóculo está sendo usado ou não pelo usuário
+    public static bool zoomIn = false;
+
+    //método responsável por iniciar os métodos de eventos disparados quando o usuário segura o binóculo
+    protected virtual void OnEnable()
+    {
+        //obtendo o objeto de interação usado no simulador a partir do script usado no componente da biblioteca (VRTK)
+        linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+        //Caso exista, vamos obter os eventos de segurar ou não o binóculo
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectGrabbed += InteractableObjectGrabbed;
+            linkedObject.InteractableObjectUngrabbed += InteractableObjectUngrabbed;
+        }
+    }
+
+    //método responsável por remover os métodos de eventos disparados quando o usuário segura ou solta o binóculo
+    protected virtual void OnDisable()
+    {
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectGrabbed -= InteractableObjectGrabbed;
+            linkedObject.InteractableObjectUngrabbed -= InteractableObjectUngrabbed;
+        }
+    }
+
+    //método disparado quando o binóculo é segurado pelo usuário
+    protected virtual void InteractableObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        //aqui desativamos a fixação do binóculo acima do peitoral do usuário
+        transformFollow.followsPosition = false;
+        transformFollow.followsRotation = false;
+    }
+
+    //método disparado quando o binóculo é solto pelo usuário
+    protected virtual void InteractableObjectUngrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        //aqui ativamos a fixação do binóculo acima do peitoral do usuário
+        transformFollow.followsPosition = true;
+        transformFollow.followsRotation = true;
+    }
+
+    //método disparado toda vez que o binóculo colide com algo dentro do simulador
+    private void OnTriggerEnter(Collider collider)
+    {
+        //caso a colisão seja da cabeça do usuário no simulador
+        if (collider.gameObject == GameObject.Find("[VRTK][AUTOGEN][HeadsetColliderContainer]"))
+        {
+            //caso o zoomIn já não esteja aplicado (isso evita a repetição do código, já que o OnTriggerEnter pode ser disparados diversas vezes)
+            if (!zoomIn)
+            {
+                //modificações necessárias para a aplicação do zoom no momento da utilização do binóculo 
+                XRDevice.fovZoomFactor = 2.5f;
+                Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 10, Time.deltaTime * smoth);
+                
+                //aqui o binóculo fica "invisível", fazendo com que ele não atrapalhe a visão do usuário na sua utilização
+                GameObject.Find("BinoPart1").GetComponentInChildren<MeshRenderer>().enabled = false;
+                GameObject.Find("BinoPart2").GetComponentInChildren<MeshRenderer>().enabled = false;
+
+                //aqui ativamos a borda delimitadora do binóculo
+                binoculoCamera.SetActive(true);
+
+                //aqui as mãos ficam "invisíveis", fazendo com que ele não atrapalhe a visão do usuário na sua utilização
+                maoEsquerda.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+                maoDireita.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+                
+                //variável utilizada para informar que o binóculo está em uso
+                zoomIn = true;
+            }
+        }
+    }
+
+    //método disparado toda vez que o binóculo sai de uma colisão com algo dentro do simulador
+    private void OnTriggerExit(Collider collider)
+    {
+        //verificando se o binóculo está em uso
+        if (zoomIn)
+        {
+            //removendo modificações necessárias para a aplicação do zoom no momento da utilização do binóculo 
+            XRDevice.fovZoomFactor = 1f;
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60, Time.deltaTime * smoth);
+
+            //revertendo a questão do binóculo ficar "invisível"
+            GameObject.Find("BinoPart1").GetComponentInChildren<MeshRenderer>().enabled = true;
+            GameObject.Find("BinoPart2").GetComponentInChildren<MeshRenderer>().enabled = true;
+
+            //desativando a borda delimitadora do binóculo
+            binoculoCamera.SetActive(false);
+
+            //revertendo a questão das mãos ficarem "invisíveis"
+            maoEsquerda.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+            maoDireita.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+
+            //variável utilizada para informar que o binóculo não está em uso
+            zoomIn = false;
+        }
+    }
+}
